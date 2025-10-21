4db1107e90f2bcba026e4879a5dc7a875321c633
Lidiia Makarchuk <87641510+norilanda@users.noreply.github.com>
2025-10-21 22:17:56 +0200

    Quartz sync: Oct 21, 2025, 10:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Obsidian for Documentation",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
